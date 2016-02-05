94308245409f058757247386c6e75fe87c43c943
Vijay Thangavel <vthangavel@sapient.com>
2016-02-05 18:49:10 +0530

    User Form.

--- assets/js/components/formUser/scripts/components/formUser.Component.js
+++ assets/js/components/formUser/scripts/components/formUser.Component.js
@@ -15,7 +15,39 @@ var FormUser = React.createClass({
     }
   },
   render: function(){
-    return <div>formUser Component render Code</div>
+    return (
+    	<div className="userForm">
+			<form>
+				<div className="row">
+					<div classNme="input-field col s12">
+						<label htmlFor="form-user-question">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </label>
+					</div>
+				</div>
+				<div className="row">
+					<div classNme="input-field col s12">
+						<label htmlFor="form-user-comment">Comments</label>
+						<textarea id="form-user-comment" className="materialize-textarea" length="120"></textarea>
+					</div>
+				</div>
+				<div className="row">
+					<div className="file-field input-field col s12">
+						<div className="btn">
+							<span>Zip file </span>
+							<input type="file" multiple />
+						</div>
+						<div className="file-path-wrapper">
+							<input className="file-path validate" type="text" placeholder="Upload one or more files" />
+						</div>
+						</div>
+				</div>
+				<div className="row">
+					<div className="col s12">
+						<button className="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+					</div>
+				</div>
+			</form>
+    	</div>
+    );
   }
 });
 
